d0c5182b4a7852be2abef5fdf03d03e2217f770c
Daniel <daniel847@tutanota.com>
2022-03-27 13:12:38 +0300

    fix: JS

--- script/index.js
+++ script/index.js
@@ -1,9 +1,25 @@
-const openformButton = document.querySelector(".popOpen");
-const closeformButton = document.querySelector(".popup__content::after");
-const popPage = document.querySelector('.popop')
+const openformButton = document.querySelector(".text__edit");
+const closeformButton = document.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
+
+let nameInput = document.querySelector(".form__input-name");
+let occupationInput = document.querySelector(".form__input-occu");
+let textName = document.querySelector(".text__name");
+let occupation = document.querySelector(".text__occu");
 
 function toggleform() {
-  popPage.classList.toggle(".pop_visi");
+  nameInput.value = textName.textContent;
+  occupationInput.value = occupation.textContent;
+  popup.classList.toggle("popup__open");
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  textName.textContent = nameInput.value;
+  occupation.textContent = occupationInput.value;
+  popup.classList.toggle("popup__open");
 }
 
 openformButton.addEventListener("click", toggleform);
+closeformButton.addEventListener("click", toggleform);
+popup.addEventListener("submit", submitForm);
