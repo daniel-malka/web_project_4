b3a6d2673616cf21d1799625b345d472e4c49e1e
Daniel <daniel847@tutanota.com>
2022-03-29 19:54:03 +0300

    fix: JS fonts buttons

--- script/index.js
+++ script/index.js
@@ -1,26 +1,30 @@
-const openformButton = document.querySelector(".text__edit");
-const closeformButton = document.querySelector(".popup__close");
+const openformButton = document.querySelector(".text__button_type_edit");
+const closeformButton = document.querySelector(".popup__button_type_close");
 const popup = document.querySelector(".popup");
 const popForm = document.getElementById("form");
 
-let nameInput = document.querySelector(".form__input-name");
-let occupationInput = document.querySelector(".form__input-occu");
+let nameInput = document.querySelector(".form__input_type_name");
+let occupationInput = document.querySelector(".form__input_type_occu");
 let textName = document.querySelector(".text__name");
 let occupation = document.querySelector(".text__occu");
 
-function toggleform() {
+function openform() {
   nameInput.value = textName.textContent;
   occupationInput.value = occupation.textContent;
-  popup.classList.toggle("popup__open");
+  popup.classList.add("is__open");
+}
+
+function closeform() {
+  popup.classList.remove("is__open");
 }
 
 function submitForm(e) {
   e.preventDefault();
   textName.textContent = nameInput.value;
   occupation.textContent = occupationInput.value;
-  popup.classList.toggle("popup__open");
+  popup.classList.remove("is__open");
 }
 
-openformButton.addEventListener("click", toggleform);
-closeformButton.addEventListener("click", toggleform);
+openformButton.addEventListener("click", openform);
+closeformButton.addEventListener("click", closeform);
 popForm.addEventListener("submit", submitForm);
