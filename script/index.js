7a3bc5d45702f168f8813fbee3e5ce945cbac7bf
Daniel <daniel847@tutanota.com>
2022-04-08 00:05:14 +0300

    5th project: improvment/ JS comments/imageAppend.css

--- script/index.js
+++ script/index.js
@@ -1,13 +1,24 @@
+//form form
+
+const popForm = document.querySelector("form");
+
+//pop box toggle target
+
+const popup = document.querySelector(".popup");
+
+//form buttons
+
 const openformButton = document.querySelector(".text__edit");
 const closeformButton = document.querySelector(".popup__button");
-const popup = document.querySelector(".popup");
-const popForm = document.getElementById("form");
+
+//form inputs
 
 let nameInput = document.querySelector(".form__input_type_name");
 let occupationInput = document.querySelector(".form__input_type_occu");
 let textName = document.querySelector(".text__name");
 let occupation = document.querySelector(".text__occu");
 
+//form open/close/submit
 const openform = () => {
   nameInput.value = textName.textContent;
   occupationInput.value = occupation.textContent;
@@ -25,16 +36,58 @@ const submitForm = (e) => {
   popup.classList.remove("popup_open");
 };
 
+//event listeners form
 openformButton.addEventListener("click", openform);
+
 closeformButton.addEventListener("click", closeform);
+
 popForm.addEventListener("submit", submitForm);
+
+//plus box buttons
+
+const plusBoxOpen = document.querySelector(".top__plus-box");
+const plusBoxClose = document.querySelector(".plusPopup__button");
+
+//plus box toggle target
+
+const plusPopup = document.querySelector(".plusPopup");
+
+//image/title inputs
+
+let titleInput = document.querySelector(".title__input_type_title");
+let linkInput = document.querySelector(".link__input_type_link");
+//image create
+const addImageButton = document.querySelector(".plusPopup__button-create");
+//image pop open/close
+
+const imageAppendOpen = () => {
+  plusPopup.classList.add("plusPopup_open");
+};
+
+const imageAppendClose = () => {
+  plusPopup.classList.remove("plusPopup_open");
+};
+// const imageAppend;
+//this function needs to:
+// 1 append an li.gallery__item to ul.gallery
+// 2 take the titleInput and set it = to .desc__text
+// 3 take the linkInput set it to .gallery__img.src
+//event listener plus box
+// const imageRemove;
+// this function needs to delete the target's.parent
+
+plusBoxOpen.addEventListener("click", imageAppendOpen);
+plusBoxClose.addEventListener("click", imageAppendClose);
+addImageButton.addEventListener("click");
+//like buttons
 
 const likeButton = document.querySelectorAll(".desc__button");
 
-for (let i = 0; i < likeButton.length; i++) {
-  let like = likeButton[i];
+//like event listener
+
+likeButton.forEach((heart) => {
   const liked = () => {
-    like.classList.toggle("button_liked");
+    heart.classList.toggle("button_liked");
   };
-  like.addEventListener("click", liked);
-}
+  heart.addEventListener("click", liked);
+});
