ffa8c20907354c5569ffb33bfecaad94365db6a4
Daniel <daniel847@tutanota.com>
2022-04-10 22:47:51 +0300

    feat: zoompopup

--- script/index.js
+++ script/index.js
@@ -1,16 +1,19 @@
 const popup = document.querySelector(".popup");
-
 const popForm = document.querySelector(".form");
+//open popup
 const editButton = document.querySelector(".text__edit");
-const closeEditButton = document.querySelector(".popup__close");
-
 const plusBoxOpen = document.querySelector(".top__plus-box");
+//close popup
+const closeEditButton = document.querySelector(".popup__close");
+const zoomClose = document.querySelector(".zoom__close");
 const plusBoxClose = document.querySelector(".popup__close");
 
+//sumbit popup
 let saveCreate = document.querySelector(".form__button");
 
 let galleryUl = document.querySelector(".gallery");
 let galleryLi = document.querySelector(".gallery__item");
+let galleryImg = document.querySelectorAll(".gallery__img");
 
 let nameInput = document.querySelector(".form__input_type_name");
 let occupationInput = document.querySelector(".form__input_type_occu");
@@ -21,56 +24,67 @@ let occupation = document.querySelector(".text__occu");
 
 let Gallery = [
   {
-    title: "Kenai Fjordlinkational Park",
+    title: "Kenai Fjords interational Park",
     link: "../images/KenaiFjords.jpg",
+    alt: "frozen lake, Kenai Fjords",
   },
   {
     title: "Yellowlinkne National Park",
     link: "../images/YellowStone2.jpg",
+    alt: "hot springs view Yellowstone park",
   },
   {
     title: "Niagara Falls",
     link: "../images/niagra_falls2.jpg",
+    alt: "niagra falls bird view",
   },
   {
     title: "Zion National Park",
     link: "../images/zion_Park2.jpg",
+    alt: "deset hill zion park",
   },
   {
     title: "Kauai islands",
     link: "../images/kauaii.jpg",
+    alt: "kauai horizon view",
   },
   {
     title: "Grand Canyon",
     link: "../images/GrandCanyon.jpg",
+    alt: "grand canyon desert view",
   },
 ];
 
 Gallery.forEach((i) => {
   const galleryItem = document.querySelector("#gallery__item").content;
   const galleryLi = galleryItem.querySelector(".gallery__item").cloneNode(true);
-
-  //this f iterates appends each template li to ul
-
-  let itemTitle = galleryLi.querySelector(".desc__text");
   let itemImage = galleryLi.querySelector(".gallery__img");
-  const galleryImgAll = galleryLi.querySelectorAll(".gallery__img");
+  let itemTitle = galleryLi.querySelector(".desc__text");
   itemImage.src = i.link;
+  itemImage.alt = i.alt;
   itemTitle.textContent = i.title;
+
   galleryUl.append(galleryLi);
-  galleryImgAll.forEach((img) => {
-    let zoomContent = document.querySelector(".zoom__img");
-    img.addEventListener("click", function (event) {
-      zoomContent.src = event.target.src;
-      openImg();
-    });
-  });
 });
 
 let itemBin = document.querySelectorAll(".gallery__bin");
 itemBin.forEach((bin) => {
   bin.addEventListener("click", function (event) {
     event.target.parentElement.remove();
+  });
+});
+
+const galleryImgAll = document.querySelectorAll(".gallery__img");
+galleryImgAll.forEach((img) => {
+  img.addEventListener("click", function (event) {
+    const zoomContent = document.querySelector(".zoom");
+    const zoomImg = document.querySelector(".zoom__img");
+    const zoomAlt = document.querySelector(".zoom__alt");
+    let itemTitle = document.querySelector(".desc__text");
+
+    zoomContent.classList.add("zoom_open");
+    zoomImg.src = event.target.src;
+    zoomAlt.textContent = itemTitle.textContent;
   });
 });
 let heartLike = document.querySelectorAll(".desc__heart");
@@ -80,7 +94,10 @@ heartLike.forEach((heart) => {
   });
 });
 //function: form / gallery edit popup
-
+const imgToggle = () => {
+  const zoomPop = document.querySelector(".zoom");
+  zoomPop.classList.toggle("zoom_open");
+};
 const openform = () => {
   popupTitle.textContent = "Edit Profile";
   saveCreate.textContent = "Save";
@@ -123,7 +140,7 @@ let saveEdit = (e) => {
     let itemBin = galleryLi.querySelectorAll(".gallery__bin");
     let heartLike = galleryLi.querySelectorAll(".desc__heart");
     itemTitle.textContent = nameInput.value;
-    itemImage.src = `"${occupationInput.value}"`;
+    itemImage.src = occupationInput.value;
     galleryUl.prepend(galleryLi);
     closeform();
     itemBin.forEach((bin) => {
@@ -136,6 +153,19 @@ let saveEdit = (e) => {
         event.target.classList.toggle("button_liked");
       });
     });
+    const galleryImgAll = document.querySelectorAll(".gallery__img");
+    galleryImgAll.forEach((img) => {
+      img.addEventListener("click", function (event) {
+        const zoomContent = document.querySelector(".zoom");
+        const zoomImg = document.querySelector(".zoom__img");
+        const zoomAlt = document.querySelector(".zoom__alt");
+        let itemTitle = document.querySelector(".desc__text");
+
+        zoomContent.classList.add("zoom_open");
+        zoomImg.src = event.target.src;
+        zoomAlt.textContent = itemTitle.textContent;
+      });
+    });
   }
 };
 
@@ -143,23 +173,7 @@ saveCreate.addEventListener("click", saveEdit);
 editButton.addEventListener("click", openform);
 plusBoxOpen.addEventListener("click", galleryEditOpen);
 closeEditButton.addEventListener("click", closeform);
-
-const zoomed = document.querySelector(".zoom");
-let galleryImgAll = document.querySelectorAll(".gallery__img");
-let galleryImg = document.querySelector(".gallery__img");
-
-const openImg = () => {
-  zoomed.classList.add("zoom_open");
-};
-
-galleryImgAll.forEach((img) => {
-  let zoomContent = document.querySelector(".zoom__img");
-  img.addEventListener("click", function (event) {
-    zoomContent.src = event.target.src;
-    openImg();
-  });
+galleryImg.forEach((img) => {
+  img.addEventListener("click", imgOpen);
 });
-
-zoomed.addEventListener("click", function (event) {
-  event.target.classList.remove("zoom_open");
-});
+zoomClose.addEventListener("click", imgToggle);
