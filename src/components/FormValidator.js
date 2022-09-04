1e1c5a3192cbbaa88d6ff96b73158a8f79c4bb36
Daniel <daniel847@tutanota.com>
2022-09-04 22:55:26 +0300

    after itter

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -2,61 +2,74 @@ export class FormValidator {
   constructor(settings, formElement) {
     this._settings = settings;
     this._formElement = formElement;
-    this._allInputs = Array.from(
-      this._formElement.querySelectorAll(this._settings.inputSelector)
-    );
+    this._allInputs = [
+      ...this._formElement.querySelectorAll(this._settings.inputSelector),
+    ];
+
     this._buttonEl = this._formElement.querySelector(
       this._settings.buttonSelector
     );
   }
+
   resetValidation = () => {
     this._hideAllErrors();
-    this._formElement.reset();
     this._disableButton();
   };
+
   enableValidation = () => {
     this._formElement.addEventListener("submit", (evt) => evt.preventDefault());
     this._setEventListeners();
   };
+
   _setEventListeners = () => {
     this._allInputs.forEach((inputEl) => {
       inputEl.addEventListener("input", () => {
         this._checkInputValidity(inputEl);
-        this._toggleButton(this._allInputs);
+        this._toggleButton();
       });
     });
   };
+
   _hideAllErrors = () => {
     this._allInputs.forEach((input) => {
       this._hideInputError(input);
     });
   };
+
   _disableButton = () => {
     const { buttonDisable } = this._settings;
     this._buttonEl.classList.add(buttonDisable);
     this._buttonEl.disabled = true;
   };
+
   _showInputError = (inputEl, errorMessage) => {
     const { inputErrorClass, spanErrorClass } = this._settings;
     this._errorDynamicSpan = this._formElement.querySelector(
       `.fieldset__error-type-${inputEl.id}`
     );
+
     inputEl.classList.add(inputErrorClass);
     this._errorDynamicSpan.textContent = errorMessage;
     this._errorDynamicSpan.classList.add(spanErrorClass);
   };
+
   _hideInputError = (inputEl) => {
     const { inputErrorClass, spanErrorClass } = this._settings;
     this._errorDynamicSpan = this._formElement.querySelector(
       `.fieldset__error-type-${inputEl.id}`
     );
+
     inputEl.classList.remove(inputErrorClass);
     this._errorDynamicSpan.classList.remove(spanErrorClass);
     this._errorDynamicSpan.textContent = "";
   };
-  _toggleButton = (inputs) => {
+
+  _checkFormValidity = () =>
+    this._allInputs.every((input) => input.validity.valid);
+
+  _toggleButton = () => {
     const { buttonDisable } = this._settings;
-    const isInputsValid = inputs.every((input) => input.validity.valid);
+    const isInputsValid = this._checkFormValidity();
 
     if (isInputsValid) {
       this._buttonEl.classList.remove(buttonDisable);
@@ -65,6 +78,7 @@ export class FormValidator {
       this._disableButton();
     }
   };
+
   _checkInputValidity = (inputEl) => {
     if (!inputEl.validity.valid) {
       this._showInputError(inputEl, inputEl.validationMessage);
