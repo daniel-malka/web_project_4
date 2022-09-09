4ced00a57c299bf22ca0f83270836a91ed8742b1
Daniel <daniel847@tutanota.com>
2022-09-09 13:37:01 +0300

    fix api last bug

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,17 +1,22 @@
 export default class Api {
   constructor({ baseUrl, headers }) {
+    console.log(baseUrl);
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
   getCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   setUserInfo({ name, about }) {
@@ -22,7 +27,9 @@ export default class Api {
         name: name,
         about: about,
       }),
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   addCard({ name, link }) {
@@ -33,7 +40,9 @@ export default class Api {
         name: name,
         link: link,
       }),
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   editAvatar(avatar) {
@@ -43,27 +52,35 @@ export default class Api {
       body: JSON.stringify({
         avatar: avatar,
       }),
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   deleteCard(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}`, {
       headers: this._headers,
       method: "DELETE",
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   dislikeCard(id) {
     return fetch(`${this._baseUrl}/cards/likes/${id}`, {
       headers: this._headers,
       method: "DELETE",
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 
   likeCard(id) {
     return fetch(`${this._baseUrl}/cards/likes/${id}`, {
       headers: this._headers,
       method: "PUT",
-    });
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
   }
 }
